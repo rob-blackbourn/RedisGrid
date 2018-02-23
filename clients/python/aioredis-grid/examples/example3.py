ad1bbe414eaf4258d3c191cc8305edc231a7488c
Rob Blackbourn <rob.blackbourn@cityfinancial.co.uk>
2018-02-23 08:11:41 +0000

    Added an example which targets a cluster

---
+++ clients/python/aioredis-grid/examples/example3.py
@@ -0,0 +1,19 @@
+"""Example which targets a redis cluster
+"""
+
+import asyncio
+import aioredis
+from aioredisgrid import Redis
+
+async def main():
+    redis = await aioredis.create_redis_pool(('localhost', 7000), commands_factory=Redis, encoding='UTF-8')
+
+    await redis.grid_dim("a1", 2, 3, 1, 2, 3, 4, 5, 6)
+    values = await redis.grid_dump("a1")
+    print(values)
+
+    redis.close()
+    await redis.wait_closed()
+
+if __name__ == '__main__':
+    asyncio.get_event_loop().run_until_complete(main())
